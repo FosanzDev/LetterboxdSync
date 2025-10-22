b87c261d0312f41bae818fa254a1692adcae439f
FosanzDev <me@fosanz.dev>
2025-10-22 23:39:51 +0200

    Revert changes

--- rxconfig.py
+++ rxconfig.py
@@ -4,6 +4,6 @@ from db.db_config import db_config
 config = rx.Config(
     app_name="LetterboxdSync",
     db_url=f"sqlite:///{db_config.get_sync_db_path()}",
-    api_url="https://lbsync.fosanz.dev:8300",
+    api_url="https://lbsync.fosanz.dev",
     disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
 )
